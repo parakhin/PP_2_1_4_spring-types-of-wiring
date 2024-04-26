8b957855242111ba4124aa349a9edd936352db8f
Сергей Парахин <sergei.parakhin@ngst.ru>
2024-04-26 10:17:24 +1000

    1

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -5,9 +5,11 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Egg6 {
-
-    @Autowired
     private Needle7 needle;
+    @Autowired
+    public void setNeedle(Needle7 needle) {
+        this.needle = needle;
+    }
 
     @Override
     public String toString() {
